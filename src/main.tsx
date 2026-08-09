03d1e4e9d53b0e98bb1d2a0792b33977830de283
fasciola <faisalmousa86@gmail.com>
2026-08-10 02:41:10 +0400

    Load final dark-theme contrast overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import './screenshot-palette.css'
+import './contrast-final.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
